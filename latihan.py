27b864a91c77b3f083df19fa5a40718120b5c06f
Shelyn Yaasinta <shelyntyaa18@yahoo.com>
2025-12-23 15:39:45 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px 
 
-st.title('Hello, Streamlit ')
+st.title('Hello, World ')
 st.write('This is a simple Streamlit Application.')
 st.write('Ini contoh *huruf miring*')
 st.write('Ini contoh **huruf tebal**')
